5da6331fffbbaf933d3338ff1cef4e0f6638c8e4
sbobhate <sbobhate@bu.edu>
2016-01-26 09:13:18 -0500

    Setup IDE
    Created Programming Assignment 1 and setup the IDE to include the
    openCV library.

---
+++ Programming Assignment 1/Programming Assignment 1/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Programming Assignment 1
+//
+//  Created by Shantanu Bobhate on 1/26/16.
+//  Copyright © 2016 Shantanu Bobhate. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
