57d1cb5d29c660b2a44daae5d0b52b0623722037
sbobhate <sbobhate@bu.edu>
2016-01-26 09:24:36 -0500

    Video Capture
    Wrote for capturing video from the webcam and displaying it in a window.

--- Programming Assignment 1/Programming Assignment 1/main.cpp
+++ Programming Assignment 1/Programming Assignment 1/main.cpp
@@ -10,7 +10,36 @@
 #include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    // Begin a video capture
+    cv::VideoCapture cap(0);
+    // Make sure the camera opened
+    if (!cap.isOpened()) {
+        std::cout << "Error: Could not open camera.\n";
+        return -1;
+    }
+    
+    // Create a Mat to hold a frame from the video capture
+    cv::Mat frame;
+    // Create a window to show the output
+    cv::namedWindow("Output", CV_WINDOW_NORMAL);
+    
+    // Exit when user clicks 'esc'
+    while (cv::waitKey(50) != 27) {
+        // Grab a frame
+        cap >> frame;
+        
+        // Make sure the image is not empty
+        if (frame.empty()) {
+            std::cout << "Error: Could not find a frame.\n";
+            return -1;
+        }
+        
+        cv::imshow("Output", frame);
+    }
+    
+    // Release all resources used by this window
+    cv::destroyWindow("Output");
+    
     return 0;
 }
