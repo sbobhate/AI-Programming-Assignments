9e139c068ea91259b69bf0911e66fcedbe1e03f6
sbobhate <sbobhate@bu.edu>
2016-02-07 16:43:48 -0500

    Testing Gestures

--- Programming Assignment 1/Programming Assignment 1/main.cpp
+++ Programming Assignment 1/Programming Assignment 1/main.cpp
@@ -17,9 +17,17 @@
  *  - create a graphical display that responds to the recognition
  */
 
+/*
+ * Define Macros
+ */
 #define WINDOW_HEIGHT 240
 #define WINDOW_WIDTH 320
+#define THRESHOLD 128
+#define MAX_THRESHOLD 255
 
+/*
+ * Declare Functions
+ */
 // Function to detect skin in a frame
 void mySkinDetect ( cv::Mat& src, cv::Mat& dst );
 // Function that returns the maximum of 3 integers
@@ -34,14 +42,14 @@ void myMotionEnergy ( std::vector < cv::Mat > frames, cv::Mat &dst, int threshol
 int findArea (cv::Mat& src);
 // Function to find the centroid of an object
 cv::Point findCenter (int area, cv::Mat& src);
+// Function to find the orientation of an object
+int findOrientation (cv::Mat& src, int x_bar, int y_bar);
+void putCircle (cv::Mat& src);
 
-cv::Mat fgMaskMOG2;
-cv::Ptr<cv::BackgroundSubtractor> pMOG2;
+
+
 
 int main(int argc, const char * argv[]) {
-    
-    // Create BackgroundSubtractor objects
-    pMOG2 = cv::createBackgroundSubtractorMOG2();
     
     // Begin a video capture
     cv::VideoCapture cap(0);
@@ -63,22 +71,7 @@ int main(int argc, const char * argv[]) {
     // Create a Mat to hold a frame from the video capture
     cv::Mat frame;
     
-//    sleep(0.5);
-//    cv::Mat frame0;
-//    cv::Mat base = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC1);
-//    cap.read(frame0);
-//    cv::cvtColor(frame0, frame0, CV_BGR2GRAY);
-//    for (int ii = 0; ii < WINDOW_HEIGHT; ++ii) {
-//        uchar* pixel = frame0.ptr<uchar>(ii);
-//        uchar* pixel_base = base.ptr<uchar>(ii);
-//        for (int jj = 0; jj < WINDOW_WIDTH; ++jj) {
-//            if (pixel[jj] > 128) {
-//                pixel_base[jj] = 255;
-//            } else {
-//                pixel_base[jj] = 0;
-//            }
-//        }
-//    }
+    sleep(1);
     
     // Exit when user clicks 'esc'
     while (cv::waitKey(50) != 27) {
@@ -91,52 +84,236 @@ int main(int argc, const char * argv[]) {
             return -1;
         }
         
-        /*
-         *
-         * Add code to implement objectives
-         *
-         */
+/*
+ *
+ * Add code to implement objectives
+ *
+ */
+    /*
+     * Put Circle
+     */
+//        putCircle(frame);
         
-        /*
-         * Background Subtraction
-         */
-        // Update the background model
-        pMOG2 -> apply(frame, fgMaskMOG2);
-        // Get the frame number and write it on the current frame
-        std::stringstream ss;
-        cv::rectangle(frame, cv::Point(10, 2), cv::Point(100, 20), cv::Scalar(255, 255, 255));
-        ss << cap.get(CV_CAP_PROP_POS_FRAMES);
-        std::string frameNumberString = ss.str();
-        cv::putText(frame, frameNumberString.c_str(), cv::Point(15, 15), CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
-        // Show the current frame and the fg Mask
-        cv::imshow("Frame", frame);
-        cv::imshow("Mask", fgMaskMOG2);
+    /*
+     * Skin Detection
+     */
+        cv::Mat skin_detected_frame = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC1);
+        mySkinDetect(frame, skin_detected_frame);
+        cv::imshow("Skin Detection", skin_detected_frame);
+
+    /*
+     * Denoising
+     */
+        // Method 1
+        cv::Mat filtered_image = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC1);
+        cv::blur(skin_detected_frame, filtered_image, cv::Size(10, 10));
+        cv::threshold(filtered_image, filtered_image, 200, MAX_THRESHOLD, 0);
+        cv::dilate(filtered_image, filtered_image, cv::Mat());
+        cv::imshow("Filter", filtered_image);
+
+    /*
+     * Draw Contours on Skin Detected Image
+     */
+        // Vector to hold contours
+        std::vector<std::vector<cv::Point>> contours;
+        // Vector to hold hierarchy
+        std::vector<cv::Vec4i> hierarchy;
+        // Clone original frame
+        cv::Mat skin2 = skin_detected_frame.clone();
+        // Find contours in clone
+        cv::findContours(skin2, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+        // Print out the number of contours detected
+        std::cout << "The number of contours detected is: " << contours.size() << std::endl;
+        // Create destination frame to show the contours
+        cv::Mat contour_output = cv::Mat::zeros(skin_detected_frame.size(), CV_8UC3);
+        // Find largest contour
+        int max_size = 0;
+        int idx = 0;
+        cv::Rect bounding_rect;
+        for (int i = 0; i < contours.size(); i++)
+        {
+            // Compute the area of the current contour
+            double area = contourArea(contours[i]);
+            // Find the contour with the largest area
+            if (area > max_size) {
+                max_size = area;
+                idx = i;
+                bounding_rect = cv::boundingRect(contours[i]);
+            }
+        }
+        // Draw contours
+        drawContours(contour_output, contours, idx, cv::Scalar(255, 0, 0), CV_FILLED, 8, hierarchy);
+        cv::drawContours(contour_output, contours, idx, cv::Scalar(0, 0, 255));
+        rectangle(frame,bounding_rect, cv::Scalar(0, 255, 0), 1, 8, 0);
+        
+        cv::namedWindow("Contours", CV_WINDOW_AUTOSIZE);
+        imshow("Contours", contour_output);
         
         /*
          * Compute Area
          */
-        int area = findArea(fgMaskMOG2);
-        std::cout << area << std::endl;
-        cv::Point center = findCenter(area, fgMaskMOG2);
-        cv::circle(frame, center, 1, cv::Scalar(0, 0, 255));
         
-        /*
-         * Skin Detection
-         */
-        cv::Mat skin_detected_frame = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC1);
-        mySkinDetect(frame, skin_detected_frame);
-        cv::imshow("Skin Detection", skin_detected_frame);
-        cv::Mat frame_test;
-        cap.read(frame_test);
-        cv::Mat frame2 = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC1);
-        mySkinDetect(frame_test, frame2);
-        cv::Mat diff = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC1);
-        myFrameDifferencing(skin_detected_frame, frame2, diff, 128);
-        cv::imshow("Diff", diff);
+//        int area = findArea(contour_output);
+//        std::cout << area << std::endl;
+//        cv::Point center = findCenter(area, contour_output);
+//        cv::circle(frame, center, 1, cv::Scalar(0, 0, 255));
+//        std::cout << findOrientation(contour_output, center.x, center.y) << std::endl;
         
+//        int sum_x = 0, sum_y = 0;
+//        int area = 1;
+//        if (bounding_rect.width != 0 && bounding_rect.height != 0) {
+//            cv::Mat roi = cv::Mat::zeros(bounding_rect.height, bounding_rect.width, CV_8UC1);
+//            roi = skin_detected_frame(cv::Rect(bounding_rect.x, bounding_rect.y, bounding_rect.width, bounding_rect.height));
+//            cv::imshow("ROI", roi);
+//            area = findArea(roi);
+//        }
+//
+//        for (int ii = bounding_rect.y; ii < bounding_rect.y + bounding_rect.height; ii++)
+//        {
+//            uchar* p = skin_detected_frame.ptr<uchar>(ii);
+//            for (int jj = bounding_rect.x; jj<bounding_rect.x + bounding_rect.width; jj++)
+//            {
+//                if (p[jj] == 255) {
+//                    sum_x += jj;
+//                    sum_y += ii;
+//                }
+//            }
+//        }
+//        int x_bar = 0, y_bar = 0;
+//        if (area != 0) {
+//            x_bar = sum_x / area;
+//            y_bar = sum_y / area;
+//        }
+//
+//        int top_dist = y_bar - bounding_rect.y;
+//        int bot_dist = bounding_rect.y + bounding_rect.height - y_bar;
+//        
+//        std::cout << "Top Dist: " << top_dist << "; Bot Dist: " << bot_dist << std::endl;
+//        std::cout << "Area: " << area << std::endl;
+//        if (area < 100) cv::putText(frame, "No Hand", cv::Point(10, 50), CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 255));
+//        else if (top_dist > bot_dist && top_dist - bot_dist > 25) cv::putText(frame, "Thumbs Up", cv::Point(10, 50), CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 255));
+//        else if (top_dist < bot_dist && bot_dist - top_dist > 25) cv::putText(frame, "Thumbs Down", cv::Point(10, 50), CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 255));
+//        else  cv::putText(frame, "Fist", cv::Point(10, 50), CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 255));
+//        
+//        cv::circle(frame, cv::Point(x_bar, y_bar), 2, cv::Scalar(0, 0, 255));
+        
+    /*
+     * Convex Hull and Convexity Defects
+     */
+        // Find the convex hull object for each contour
+        /* METHOD 1*/
         /*
-         * Hand Gestures
-         */
+        std::vector < std::vector < cv::Point > > hull (contours.size());
+        std::vector < std::vector < int > > hullI (contours.size());
+        std::vector < std::vector < cv::Vec4i > > defects (contours.size());
+        
+        for (int ii = 0; ii < contours.size(); ++ii) {
+            cv::convexHull(cv::Mat(contours[ii]), hull[ii]);
+            cv::convexHull(cv::Mat(contours[ii]), hullI[ii]);
+            // More than 3 indicies are needed
+            if (hullI.size() > 3) {
+                cv::convexityDefects(contours[ii], hullI[ii], defects[ii]);
+            }
+        }
+        cv::drawContours(frame, hull, 0, cv::Scalar(255, 0, 0));
+        cv::imshow("Hull", contour_output);
+        */
+        /* METHOD 2 */
+        std::vector < std::vector < cv::Point > > hull (1);
+        std::vector < int > hullI;
+        std::vector < cv::Vec4i > defects;
+        if (!contours.empty()) {
+            cv::convexHull(cv::Mat(contours[idx]), hull[0]);
+            cv::convexHull(cv::Mat(contours[idx]), hullI);
+        }
+        // More than 3 indicies are needed
+        if (hullI.size() > 3) {
+            cv::convexityDefects(contours[idx], hullI, defects);
+        }
+        cv::drawContours(frame, hull, 0, cv::Scalar(255, 0, 0));
+        cv::imshow("Hull", contour_output);
+        
+    /*
+     * Draw Convexity Defects
+     */
+        /* METHOD 1 */
+        /*
+        for (int ii = 0; ii < contours.size(); ++ii)  {
+            for (const cv::Vec4i& v : defects[ii]) {
+                float depth = v[3] / 256;
+                if (depth > 10) {
+                    int start_idx = v[0];
+                    cv::Point start = contours[ii][start_idx];
+                    int end_idx = v[1];
+                    cv::Point end = contours[ii][end_idx];
+                    int far_idx = v[2];
+                    cv::Point far = contours[ii][far_idx];
+                    
+                    cv::line(frame, start, end, cv::Scalar(0, 150, 0));
+                    cv::line(frame, start, far, cv::Scalar(0, 150, 0));
+                    cv::line(frame, end, far, cv::Scalar(0, 150, 0));
+                    cv::circle(frame, far, 4, cv::Scalar(0, 150, 0));
+                }
+            }
+        }
+        */
+        /* METHOD 2 */
+        int finger_count = 0;
+        for (const cv::Vec4i& v : defects) {
+            float depth = v[3] / 256;
+            if (depth > 20 && depth < 80) {
+                int start_idx = v[0];
+                cv::Point start = contours[idx][start_idx];
+                int end_idx = v[1];
+                cv::Point end = contours[idx][end_idx];
+                int far_idx = v[2];
+                cv::Point far = contours[idx][far_idx];
+                    
+//                cv::line(frame, start, end, cv::Scalar(0, 255, 0), 2);
+                cv::line(frame, start, far, cv::Scalar(0, 255, 0), 2);
+                cv::line(frame, end, far, cv::Scalar(0, 255, 0), 2);
+                cv::circle(frame, start, 4, cv::Scalar(100, 0, 255), 2);
+                finger_count++;
+            }
+        }
+        std::cout << finger_count << std::endl;
+        std::stringstream ss;
+        ss << finger_count;
+        cv::putText(frame, ss.str(), cv::Point(10, 20), CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+        
+//        std::vector < int > h;
+//        std::vector < cv::Vec4i > defects;
+//        cv::convexHull(contours[idx], h);
+//        cv::convexityDefects(contours[idx], h, defects);
+//        
+//        for (int ii = 0; ii < defects.size(); ++ii) {
+//            cv::Vec4i value = defects[ii];
+//            int s = value[0];
+//            int e = value[1];
+//            cv::line(frame, contours[s][0], contours[e][0], cv::Scalar(150, 0, 150));
+//        }
+        
+    /*
+     * Hand Gestures
+     */
+//        int fingerCount = 1;
+//        for (int i = 0; i< defects.size(); i++){
+//            int start_index = defects[i][0];
+//            CvPoint start_point = contours[idx][start_index];
+//            int end_index = defects[i][1];
+//            CvPoint end_point = contours[idx][end_index];
+//            double d1 = (end_point.x - start_point.x);
+//            double d2 = (end_point.y - start_point.y);
+//            double distance = sqrt((d1*d1)+(d2*d2));
+//            int depth_index = defects[i][2];
+//            int depth =  defects[i][3]/1000;
+//            
+//            if (depth > 10 && distance > 2.0 && distance < 200.0){
+//                fingerCount ++;
+//            }
+//        }
+//        
+//        std::cout << fingerCount << std::endl;
         
         // Show the images
         cv::imshow("Original", frame);
@@ -192,10 +369,14 @@ int myMin(int a, int b, int c) {
 
 // Function to find the difference between 2 frames
 void myFrameDifferencing (cv::Mat& first, cv::Mat& second, cv::Mat& dst, int threshold) {
+    cv::Mat gray_first = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC1);
+    cv::Mat gray_second = cv::Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC1);
+    cv::cvtColor(first, gray_first, CV_BGR2GRAY);
+    cv::cvtColor(second, gray_second, CV_BGR2GRAY);
     // Iterate through each pixel sequentially
     for (int ii = 0; ii < WINDOW_HEIGHT; ++ii) {
-        uchar* pixel_1 = first.ptr<uchar>(ii);
-        uchar* pixel_2 = second.ptr<uchar>(ii);
+        uchar* pixel_1 = gray_first.ptr<uchar>(ii);
+        uchar* pixel_2 = gray_second.ptr<uchar>(ii);
         uchar* pixel_dst = dst.ptr<uchar>(ii);
         for (int jj = 0; jj < WINDOW_WIDTH; ++jj) {
             // Compute the intensities
@@ -238,6 +419,7 @@ void myMotionEnergy (std::vector < cv::Mat > frames, cv::Mat &dst, int threshold
     }
 }
 
+// Function to find the area of an object
 int findArea (cv::Mat& src) {
     int area = 0;
     for (int ii = 0; ii < WINDOW_HEIGHT; ++ii) {
@@ -251,6 +433,7 @@ int findArea (cv::Mat& src) {
     return area;
 }
 
+// Function to find the center of an object
 cv::Point findCenter (int area, cv::Mat& src) {
     int sum_x = 0, sum_y = 0;
     for (int ii = 0; ii< WINDOW_HEIGHT; ii++)
@@ -272,10 +455,33 @@ cv::Point findCenter (int area, cv::Mat& src) {
     return cv::Point (x_bar, y_bar);
 }
 
+// Function to find the orientation of an object
+int findOrientation (cv::Mat& src, int x_bar, int y_bar) {
+    int alpha = 0, beta = 0, gamma = 0;
+    for (int ii = 0; ii < WINDOW_HEIGHT; ++ii) {
+        for (int jj = 0; jj < WINDOW_WIDTH; ++jj) {
+            int s = ii - x_bar;
+            int t = jj - y_bar;
+            alpha += s*s;
+            beta += s*t;
+            gamma = t*t;
+        }
+    }
+    return atan(beta / (alpha - gamma));
+}
+
+void putCircle (cv::Mat& src) {
+    int rand_x = rand() % WINDOW_WIDTH;
+    int rand_y = rand() % WINDOW_HEIGHT;
+    std::cout << "(" << rand_x << ", " << rand_y << ")" << std::endl;
+    cv::circle(src, cv::Point(rand_x, rand_y), 30, cv::Scalar(255, 255, 150));
+}
+
 /*
  * References:
- *  - http://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
- *  - http://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html
- *  - http://docs.opencv.org/master/d1/dc5/tutorial_background_subtraction.html#gsc.tab=0
+ *  - Motion Detection and Tracking: http://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
+ *  - Common OpenCV Functions: http://docs.opencv.org/2.4/modules/imgproc/doc/miscellaneous_transformations.html
+ *  - OpenCV Background Subtraction Methods: http://docs.opencv.org/master/d1/dc5/tutorial_background_subtraction.html#gsc.tab=0
  *  - Hand Gesture Recognition using Convex Hull: http://anikettatipamula.blogspot.ro/2012/02/hand-gesture-using-opencv.html
+ *  - How to Draw Convexity Defects: http://stackoverflow.com/questions/31354150/opencv-convexity-defects-drawing
  */
